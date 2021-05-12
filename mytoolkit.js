ab33fa80858300dc079ff24ff6c15dc45e8efcb1
Kevin Huynh <kevinhuynh914@gmail.com>
2021-05-12 14:41:50 -0700

    first commit

---
+++ mytoolkit.js
@@ -0,0 +1,44 @@
+// File name: mytoolkit.js
+
+import { SVG } from "./svg.min.js";
+
+SVG.on(document, "DOMContentLoaded", function () {
+    var btn = new MyToolkit.Button;
+
+    btn.onclick(function () {
+        console.log("clicked");
+    });
+});
+
+var MyToolkit = (function () {
+    var Button = function () {
+        var draw = SVG().addTo("body").size("100%", "100%");
+        var rect = draw.rect(100, 50).fill("red");
+        var clickEvent = null;
+
+        rect.mouseover(function () {
+            this.fill({ color: "blue" });
+        });
+        rect.mouseout(function () {
+            this.fill({ color: "red" });
+        });
+        rect.mouseup(function () {
+            this.fill({ color: "red" });
+        });
+        rect.click(function (event) {
+            this.fill({ color: "pink" });
+            if (clickEvent != null) clickEvent(event);
+        });
+        return {
+            move: function (x, y) {
+                rect.move(x, y);
+            },
+            onclick: function (eventHandler) {
+                clickEvent = eventHandler;
+            },
+        };
+    };
+    return { Button };
+})();
+
+// export { MyToolkit };
