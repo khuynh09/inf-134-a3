f1a23a00575a2b3d4257269ae990f0d08f35a714
Kevin Huynh <kevinhuynh914@gmail.com>
2021-05-14 02:32:23 -0700

    finished button

--- mytoolkit.js
+++ mytoolkit.js
@@ -5,28 +5,43 @@ import { SVG } from "./svg.min.js";
 SVG.on(document, "DOMContentLoaded", function () {
     var btn = new MyToolkit.Button;
 
-    btn.onclick(function () {
-        console.log("clicked");
+    btn.setText("My Button")
+    btn.onclick(function (e) {
+        console.log(e);
+        console.log("Press: Execute")
+        console.log("Press: Idle")
     });
 });
 
 var MyToolkit = (function () {
+
+    
     var Button = function () {
+
+        
         var draw = SVG().addTo("body").size("100%", "100%");
-        var rect = draw.rect(100, 50).fill("red");
+        var rect = draw.rect(100, 50).fill("#7892c2");
+        var text = draw.text("Button");
+        text.font({fill:"#ffffff", size: 16, family: 'Helvetica'});
+        var len = text.length(); 
+        text.attr({x:(100-len)/2, y:(50-28)/2})
         var clickEvent = null;
 
+
+
         rect.mouseover(function () {
-            this.fill({ color: "blue" });
+            this.fill({ color: "#476e9e" });
+            console.log("State: Idle")
         });
         rect.mouseout(function () {
-            this.fill({ color: "red" });
+            this.fill({ color: "#7892c2" });
+            console.log("State: Idle")
+
         });
-        rect.mouseup(function () {
-            this.fill({ color: "red" });
-        });
+
         rect.click(function (event) {
-            this.fill({ color: "pink" });
+            this.fill({ color: "#7892c2" });
+            console.log("State: Pressed")
             if (clickEvent != null) clickEvent(event);
         });
         return {
@@ -36,6 +51,12 @@ var MyToolkit = (function () {
             onclick: function (eventHandler) {
                 clickEvent = eventHandler;
             },
+            setText: function(t){
+                text.text(t)
+                var len = text.length(); 
+                text.attr({x:(100-len)/2, y:(50-28)/2})
+
+            }
         };
     };
     return { Button };
